8ee157cb67e6ef896357f556715aa99e16017bce
zehrahayirci <zehrahayirci@gmail.com>
2022-12-07 22:21:08 +0100

    Day 7

---
+++ advent7.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from os import path
+from collections import defaultdict
+
+with open(path.join(path.dirname(__file__), "demo7.txt")) as f:
+    dirs = defaultdict(int)
+    cwd = []
+    for line in f.read().splitlines():
+        if line.startswith("$ cd"):
+            ls = False
+            d = line[5:]
+            if d == "..":
+                cwd.pop()
+            else:
+                cwd.append(d)
+        elif line.startswith("$ ls"):
+            continue
+        else:
+            try:
+                dirs["/".join(cwd)] += int(line.split()[0])
+            except ValueError:
+                pass
+
+print(dirs)
+print(sorted(dirs.keys(), key=lambda x: x.count("/"), reverse=True))
+for d in sorted(dirs.keys(), key=lambda x: x.count("/"), reverse=True):
+    dirs["/".join(d.split("/")[:-1])] += dirs[d]
+
+print("Part 1:", sum(s for s in dirs.values() if s <= 100_000))
+
+free = 70_000_000 - dirs["/"]
+needed = 30000000 - free
+print("Part 2:", min(v for v in dirs.values() if v > needed))
